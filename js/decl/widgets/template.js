c96c4aa3d611116c40b9b598871b320d2a970732
Torsten Sachse <torsten.sachse@nokia.com>
2011-06-15 17:51:30 +0200

    Adding templage widget.

---
+++ js/decl/widgets/template.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * @widget: template
+ */
+
+var templates = {};
+
+decl.widget("template", function(node) {
+    var name = node.getAttribute("template");
+    templates[name] = node;
+    node.parentNode && node.parentNode.removeChild(node);
+}, 1);
+
+decl.widget("use", function(node) {
+});
