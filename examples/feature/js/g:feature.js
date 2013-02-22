d2b278264189913c31f56143f4acd20222c28f14
Torsten Sachse <torsten.sachse@nokia.com>
2013-02-22 16:37:59 +0100

    Improved g-feature example.

--- examples/feature/js/g:feature.js
+++ examples/feature/js/g:feature.js
@@ -7,15 +7,18 @@ gaia.directive("g:feature", function(node, next) {
     if (node.hasAttribute("g:feature")) {
         var expr = gaia.parse(node.getAttribute("g:feature"));
         node.removeAttribute("g:feature");
-        console.log("~ [feature]", expr.$source);
+        var name = node.getAttribute("name");
+        node.removeAttribute("name");
+        console.log("~ [feature]", expr.$source, "as", name);
         next(function(n, next) {
             var scope = this
                 , feature = expr(this)
                 ;
             console.log("~ including feature:", expr.$source);
             if (feature) {
-                if (!feature.useParentScope) scope = gaia.scope(scope);
+                /*if (!feature.useParentScope) */scope = gaia.scope(scope);
                 feature.call(scope, n);
+                if (name) this[name] = scope;
             } else {
                 console.warn("~ [feature]", expr.$source, "not available");
             }
